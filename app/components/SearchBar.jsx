f8d0040559bd9c193786f5605f87c96494d8f43e
Rob Pataki <rob@handcrafteddigital.london>
2016-11-30 13:40:08 +0000

    Add states to SearchBar

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -6,7 +6,26 @@ export class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      focus: false,
+    };
+
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this.onCloseClick = this.onCloseClick.bind(this);
+  }
+
+  onFocus(e) {
+    this.setState({
+      focus: true,
+    });
+  }
+
+  onBlur() {
+    this.setState({
+      focus: false,
+    });
   }
 
   onSearchTextChange() {
@@ -16,14 +35,24 @@ export class SearchBar extends React.Component {
     dispatch(actions.setSearchText(searchText));
   }
 
+  onCloseClick() {
+    const { dispatch } = this.props;
+    dispatch(actions.setSearchText(''));
+  }
+
   render() {
     const { dispatch, searchText } = this.props;
+    let className = this.state.focus ? "search-bar search-bar--focus" : "search-bar";
+
+    if(searchText.length) {
+      className += " search-bar--has-text";
+    }
 
     return (
-      <div className="search-bar">
+      <div className={ className }>
         <i className="search-bar__icon search-bar__icon--magnify"></i>
-        <input className="search-bar__input" type="text" name="searchText" ref="searchText" placeholder="Search" value={ searchText } onChange={ this.onSearchTextChange } />
-        <i className="search-bar__icon search-bar__icon--close"></i>
+        <input className="search-bar__input" type="text" name="searchText" ref="searchText" placeholder="Search" value={ searchText } onChange={ this.onSearchTextChange } onFocus={ this.onFocus } onBlur={ this.onBlur } />
+        <i className="search-bar__icon search-bar__icon--close" onClick={ this.onCloseClick } ></i>
       </div>
     );
   }
