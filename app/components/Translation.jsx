db73de5b5907745d870fe05438c266830698b160
Rob Pataki <rob@handcrafteddigital.london>
2016-12-03 21:12:27 +0000

    Add touch controls to translation DELETE action

--- app/components/Translation.jsx
+++ app/components/Translation.jsx
@@ -9,18 +9,20 @@ const BREWSER = require('Brewser').BREWSER;
 
 import * as actions from 'actions';
 
+const STATUS = {
+  EDIT: 'edit',
+  DELETE: 'delete',
+  STAND_BY: 'standBy',
+};
+
 // Raw component
 export class Translation extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      offsetX: 0
-    };
-
     // Generic events
-    this.onEditSelect = this.onEditSelect.bind(this);
-    this.onDeleteSelect = this.onDeleteSelect.bind(this);
+    this.handleEditButtonSelect = this.handleEditButtonSelect.bind(this);
+    this.handleDeleteButtonSelect = this.handleDeleteButtonSelect.bind(this);
 
     // Mouse events
     this.onTriggerMouseOver = this.onTriggerMouseOver.bind(this);
@@ -32,8 +34,12 @@ export class Translation extends React.Component {
     this.onTouchEnd = this.handleTouchEnd.bind(this);
 
     // Animations
-    this.panAnim = this.panAnim.bind(this);
+    this.slideBackAnim = this.slideBackAnim.bind(this);
     this.hoverAnim = this.hoverAnim.bind(this);
+
+    // Initial state
+    this.state = {};
+    this.ignoreUser = false;
   }
 
   createAnim(utils) {
@@ -108,19 +114,34 @@ export class Translation extends React.Component {
     });
   }
 
-  panAnim(utils) {
+  slideBackAnim(utils) {
     let obj = {
       x: this.offsetX,
     };
 
     this.tween = new TimelineMax({});
 
+    const callback = utils.options.callback;
+
     return this.tween
-    .to(obj, 0.9, {
+    .to(obj, 0.45, {
       x: 0,
-      ease: Expo.easeOut,
+      ease: Quart.easeOut,
+      onStart: () => {
+        this.ignoreUser = true;
+      },
       onUpdate: () => {
-        this.applyContentOffsetX(obj.x);
+        this.applyOffsetXToEl(this.contentsEl, obj.x);
+        this.applyButtonOffsetX(obj.x);
+      },
+      onComplete: () => {
+        this.offsetX = 0;
+        this.ignoreUser = false;
+        this.resetButtons();
+
+        if (callback) {
+          callback.call(this);
+        }
       }
     });
   }
@@ -145,8 +166,8 @@ export class Translation extends React.Component {
   componentDidMount() {
     // Set trigger width
     if (this.leftTrigger && this.rightTrigger) {
-      this.leftTrigger.style.maxWidth = this.editButton.outerWidth;
-      this.rightTrigger.style.maxWidth = this.deleteButton.outerWidth;
+      this.leftTrigger.style.maxWidth = this.editButton.offsetWidth;
+      this.rightTrigger.style.maxWidth = this.deleteButton.offsetWidth;
     }
   }
 
@@ -155,66 +176,72 @@ export class Translation extends React.Component {
 /////
 
   /* Generic events */
-  onDeleteSelect(e) {
-    e.preventDefault();
-
+  handleDeleteButtonSelect() {
     const { dispatch, id } = this.props;
     dispatch(actions.startDeleteTranslation(id));
   }
 
-  onEditSelect(e) {
-    e.preventDefault();
-
+  handleEditButtonSelect() {
     const { dispatch, id } = this.props;
+    console.log('EDIT is coming soon to your screen...');
   }
 
   handleTouchStart(e) {
-    if (this.tween) {
-      this.tween.kill().remove();
-      this.tween = undefined;
+    if (!this.ignoreUser) {
+      if (this.tween) {
+        this.tween.kill().remove();
+        this.tween = undefined;
+      }
+      this.clientX = e.touches[0].clientX;
+      this.clientY = e.touches[0].clientY;
+    } else {
+      this.clientX = this.clientY = undefined;
     }
-    this.clientX = e.touches[0].clientX;
-    this.clientY = e.touches[0].clientY;
   }
 
   handleTouchMove(e) {
-    const buttonActivationOffsetX = 50;
-    const maxOffsetX = 100;
+    if (!this.ignoreUser && typeof this.clientX !== 'undefined') {
+      const maxOffsetX = 150;
 
-    const clientX = e.touches[0].clientX;
-    let offsetX = clientX - this.clientX;
+      const clientX = e.touches[0].clientX;
+      let offsetX = clientX - this.clientX;
 
-    /* Prevents scrolling if the user is panning the item */
-    if (offsetX >= 20) {
-      e.preventDefault();
+      /* Prevents scrolling if the user is panning the item */
+      if (offsetX >= 20) {
+        e.preventDefault();
+      }
+
+      if (offsetX > maxOffsetX) {
+        offsetX = maxOffsetX;
+      } else if (offsetX < -1 * maxOffsetX) {
+        offsetX = -1 * maxOffsetX;
+      }
+
+      this.offsetX = offsetX;
+      this.applyOffsetXToEl(this.contentsEl, offsetX);
+      this.applyButtonOffsetX(offsetX);
     }
-
-    if (offsetX > maxOffsetX) {
-      offsetX = maxOffsetX;
-    } else if (offsetX < -1 * maxOffsetX) {
-      offsetX = -1 * maxOffsetX;
-    }
-
-    let activityHighlight = undefined;
-    if (offsetX >= buttonActivationOffsetX) {
-      activityHighlight = 'EDIT';
-    } else if(offsetX <= -1 * buttonActivationOffsetX) {
-      activityHighlight = 'DELETE';
-    }
-
-    // this.setState({
-    //   offsetX: offsetX,
-    //   activityHighlight: activityHighlight
-    // });
-
-    this.offsetX = offsetX;
-    this.applyContentOffsetX(offsetX);
   }
 
   handleTouchEnd(e) {
-    const animController = this.addAnimation(this.panAnim, {
-      offsetX: 0,
-    });
+    if (!this.ignoreUser && typeof this.clientX !== 'undefined') {
+      const cachedStatus = this.status;
+
+      // Play out animation
+      const animController = this.addAnimation(this.slideBackAnim, {
+        callback: () => {
+          // Trigger user action
+          switch(cachedStatus) {
+            case STATUS.EDIT:
+              this.handleEditButtonSelect();
+              break;
+            case STATUS.DELETE:
+              this.handleDeleteButtonSelect();
+              break;
+          }
+        }
+      });
+    }
   }
 
   /* Desktop device events */
@@ -232,13 +259,64 @@ export class Translation extends React.Component {
     });
   }
 
-  applyContentOffsetX(x) {
-    if (!this.contentsEl) {
+  resetButtons() {
+    this.buttonWrapper.className = this.buttonWrapper.className.replace(/edit|delete/gi, '')
+    this.buttonWrapper.className = this.buttonWrapper.className.replace(/^[.\s]+|[.\s]+$/g, '');
+  }
+
+  applyOffsetXToEl(el, offsetX) {
+    if (!el || typeof el.style === 'undefined') {
       return;
     }
 
-    this.contentsEl.style.transform = `translate3d(${x}px, 0, 0)`;
-    this.contentsEl.style.webKitTransform = `translate3d(${x}px, 0, 0)`;
+    el.style.transform = `translate3d(${offsetX}px, 0, 0)`;
+    el.style.webKitTransform = `translate3d(${offsetX}px, 0, 0)`;
+  }
+
+  applyButtonOffsetX(offsetX) {
+    if (!this.editButton || !this.deleteButton) {
+      return;
+    }
+
+    const buttonWidth = this.editButton.offsetWidth;
+    const buttonActivationOffsetX = 75;
+
+    if (offsetX > 0) {
+      // EDIT button
+      // Move the button with the parent
+      if (offsetX > buttonWidth) {
+        this.applyOffsetXToEl(this.editButton, offsetX - buttonWidth);
+      }
+
+      // Active?
+      if (offsetX > buttonActivationOffsetX) {
+        if (this.buttonWrapper.className.indexOf('edit') < 0) {
+          this.buttonWrapper.className += ' edit';
+          this.status = STATUS.EDIT;
+        }
+      } else {
+        this.buttonWrapper.className = this.buttonWrapper.className.replace('edit', '');
+        this.status = STATUS.STAND_BY;
+      }
+
+    } else {
+      // DELETE button
+      // Move the button with the parent
+      if (-1 * offsetX > buttonWidth) {
+        this.applyOffsetXToEl(this.deleteButton, offsetX + buttonWidth);
+      }
+
+      // Active?
+      if (-1 * offsetX > buttonActivationOffsetX) {
+        if (this.buttonWrapper.className.indexOf('delete') < 0) {
+          this.buttonWrapper.className += ' delete';
+          this.status = STATUS.DELETE;
+        }
+      } else {
+        this.buttonWrapper.className = this.buttonWrapper.className.replace('delete', '');
+        this.status = STATUS.STAND_BY;
+      }
+    }
   }
 
   renderTriggers(touchDevice) {
@@ -249,10 +327,10 @@ export class Translation extends React.Component {
     } else {
       return(
         <div className="translation__wrapper translation__wrapper--triggers">
-          <span className="translation__trigger translation__trigger--left" onClick={ this.onEditSelect } onMouseOver={ this.onTriggerMouseOver } onMouseOut={ this.onTriggerMouseOut } ref={(ref) => {
+          <span className="translation__trigger translation__trigger--left" onClick={ this.handleEditButtonSelect } onMouseOver={ this.onTriggerMouseOver } onMouseOut={ this.onTriggerMouseOut } ref={(ref) => {
             this.leftTrigger = ref;
           }}></span>
-          <span className="translation__trigger translation__trigger--right" onClick={ this.onDeleteSelect } onMouseOver={ this.onTriggerMouseOver } onMouseOut={ this.onTriggerMouseOut } ref={(ref) => {
+        <span className="translation__trigger translation__trigger--right" onClick={ this.handleDeleteButtonSelect } onMouseOver={ this.onTriggerMouseOver } onMouseOut={ this.onTriggerMouseOut } ref={(ref) => {
             this.rightTrigger = ref;
           }}></span>
         </div>
@@ -261,23 +339,10 @@ export class Translation extends React.Component {
   }
 
   render() {
-    // this.applyContentOffsetX(this.state.offsetX);
-
     const { id, expression, meaning, createdAt } = this.props;
 
-    let className = 'translation';
-
-    switch(this.state.activityHighlight) {
-      case 'EDIT':
-        className += ' translation--edit-active';
-        break;
-      case 'DELETE':
-        className += ' translation--delete-active';
-        break;
-    }
-
     return (
-      <div className={ className }>
+      <div className="translation">
         <div className="translation__wrapper translation__wrapper--contents" name="contents" ref={(ref) => {
           this.contentsEl = ref;
         }}>
@@ -285,13 +350,14 @@ export class Translation extends React.Component {
           <span className="translation__meaning">{ meaning }</span>
         </div>
 
-        <div className="translation__wrapper translation__wrapper--buttons" name="buttons">
+        <div className="translation__wrapper translation__wrapper--buttons" ref={(ref) => {
+            this.buttonWrapper = ref; }}>
           <button className="translation__button translation__button--edit" ref={(ref) => {
             this.editButton = ref;
-          }} onClick={ this.onEditSelect }></button>
+          }} onClick={ this.handleEditButtonSelect }></button>
           <button className="translation__button translation__button--delete" ref={(ref) => {
             this.deleteButton = ref;
-          }} onClick={ this.onDeleteSelect }></button>
+          }} onClick={ this.handleDeleteButtonSelect }></button>
         </div>
 
         { this.renderTriggers(BREWSER.device.touch) }
